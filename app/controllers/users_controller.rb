c6bf2aa7fb0878d7514dfb66f9f59ac9bcfa484b
Khaledsf <kbouchama@gmail.com>
2014-10-07 14:28:00 -0700

    implemented adding and viewing user records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+
+	before_action :set_user, only: [:show]
+	
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+        @user = User.new(user_params)
+
+   		respond_to do |format|
+	     	if @user.save
+	       		format.html { redirect_to @user, notice: 'User was successfully created.' }
+	       		format.json { render :show, status: :created, location: @user }
+	     	else
+	       		format.html { render :new }
+	       		format.json { render json: @user.errors, status: :unprocessable_entity }
+	     	end
+   		end
+	end
+
+	def show
+		
+	end
+
+	private
+	def user_params
+		params.require(:user)
+			.permit(:username, :password, :password_confirmation, :firstname, :lastname, :city, :bio)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
 end
