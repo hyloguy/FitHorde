3b755dfbe2593cb4a98c91f9ca0d1c33c9ac1f0c
Khaledsf <kbouchama@gmail.com>
2014-10-07 15:06:55 -0700

    implemented all seven routes for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-	before_action :set_user, only: [:show]
+	before_action :set_user, only: [:show, :update, :destroy, :edit]
 	
 	def index
 		@users = User.all
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	end
 
 	def create
-        @user = User.new(user_params)
+      @user = User.new(user_params)
 
    		respond_to do |format|
 	     	if @user.save
@@ -26,6 +26,30 @@ class UsersController < ApplicationController
 
 	def show
 		
+	end
+
+	def edit
+		
+	end
+
+	def update
+ 		respond_to do |format|
+     	if @user.update(user_params)
+       		format.html { redirect_to @user, notice: 'User was successfully updated.' }
+       		format.json { render :show, status: :ok, location: @user }
+     	else
+       		format.html { render :edit }
+       		format.json { render json: @user.errors, status: :unprocessable_entity }
+     	end
+ 		end
+	end
+
+	def destroy
+		@user.destroy
+ 		respond_to do |format|
+   		format.html { redirect_to users_url, notice: 'User was successfully deleted.' }
+   		format.json { head :no_content }
+ 		end
 	end
 
 	private
