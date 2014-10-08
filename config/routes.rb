ea830a3e0395129cdcaaba302ebb75dde4bb9071
Khaledsf <kbouchama@gmail.com>
2014-10-07 19:57:04 -0700

    implemented events and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :events
     resources :users
+    resources :bookings
 
   # Example resource route with options:
   #   resources :products do
